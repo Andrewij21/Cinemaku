d0382b58289f06a769ab4093664b55425263e9e1
Andrewij21 <andrewijaya988@gmail.com>
2023-09-28 14:53:33 +0700

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="bg-slate-800 min-h-screen">
+      <NavBar />
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { AiOutlineUser } from "react-icons/ai";
+const NavBar = () => {
+  return (
+    <header>
+      <nav className="font-mukta px-12 py-4 flex justify-between border-black outline">
+        <h1 className="text-5xl uppercase tracking-wide text-rose-600 font-bold">
+          Cinemaku
+        </h1>
+        <ul className="flex justify-center items-center gap-12 text-slate-200">
+          <li className="hover:text-rose-600 text-lg">
+            <a href="#">Home</a>
+          </li>
+          <li className="">
+            <input
+              type="text"
+              className="py-2 px-4 rounded-md outline-none text-black cursor-pointer hover:ring hover:ring-rose-600 
+               focus:ring focus:ring-rose-600 "
+              placeholder="Search film..."
+            />
+          </li>
+          <li className="text-xl cursor-pointer hover:text-rose-600">
+            <AiOutlineUser />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
