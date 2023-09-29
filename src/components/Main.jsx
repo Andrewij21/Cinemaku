db3dd8a3c331ba3bea0cd1681a837bf90d3e5d77
Andrewij21 <andrewijaya988@gmail.com>
2023-09-29 15:00:20 +0700

    feat: display data and fix responsive

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,37 +1,50 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import avanger from "../../public/avanger.jpg";
 import { getMovieTrending } from "../api/movieApi";
 
 const Main = () => {
+  const [tranding, setTrending] = useState([]);
   useEffect(() => {
+    let isMounted = false;
     getMovieTrending()
       .then((res) => {
+        isMounted = true;
         console.log(res);
+        const movies = res.data.results;
+        console.log({ movies });
+        isMounted && setTrending(movies);
       })
       .catch((e) => {
         console.log(e.toString());
       });
+
+    return () => (isMounted = false);
   }, []);
+  console.log({ tranding });
   return (
     <main className="">
       <h2 className="text-2xl text-slate-200 mb-4">Movies</h2>
-      <div className="text-white flex flex-row gap-12">
-        <div className="shadow-lg shadow-black rounded-xl overflow-hidden">
-          <img src={avanger} className="w-56" alt="" />
-          <div className="text-center py-2 leading-relaxed ">
-            <h4>Avanger</h4>
-            <p>2012-12-12</p>
-            <p>3.43</p>
-          </div>
-        </div>
-        <div className="shadow-lg shadow-black rounded-xl overflow-hidden">
-          <img src={avanger} className="w-56 mb-2" alt="" />
-          <div className="text-center leading-relaxed">
-            <h4>Avanger</h4>
-            <p>2012-12-12</p>
-            <p>3.43</p>
-          </div>
-        </div>
+      <div className="text-white flex flex-wrap  justify-start items-start gap-12 min-w-full">
+        {tranding.map((movie) => {
+          return (
+            <div
+              className="shadow-lg items-start shadow-black rounded-xl w-1/6 h-auto overflow-hidden"
+              key={movie.id}
+            >
+              <img
+                src={avanger}
+                // className=" bg-red-400 object-cover"
+                className=" w-full rounded-lg object-cover object-center"
+                alt={movie.title}
+              />
+              <div className="text-center p-4 leading-relaxed ">
+                <h4 className="">{movie.title}</h4>
+                <p>{movie.release_date}</p>
+                <p>{movie.vote_average}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </main>
   );
