cee1a3b713b9605e1cbfc55463136ddfc03dbd45
Andrewij21 <andrewijaya988@gmail.com>
2023-09-30 15:26:15 +0700

    refactor: responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,19 +6,19 @@ const NavBar = () => {
         <h1 className="text-5xl uppercase tracking-wide text-rose-600 font-bold">
           Cinemaku
         </h1>
-        <ul className="flex justify-center items-center gap-12 text-slate-200">
+        <ul className="hidden md:flex justify-center items-center gap-12 text-slate-200 cursor-pointer font-semibold">
           <li className="hover:text-rose-600 text-lg">
             <a href="#">Home</a>
           </li>
           <li className="">
             <input
               type="text"
-              className="py-2 px-4 rounded-md outline-none text-black cursor-pointer hover:ring hover:ring-rose-600 
+              className="py-2 px-4 rounded-md outline-none text-black  hover:ring hover:ring-rose-600 
                focus:ring focus:ring-rose-600 "
               placeholder="Search film..."
             />
           </li>
-          <li className="text-xl cursor-pointer hover:text-rose-600">
+          <li className="text-xl hover:text-rose-600">
             <AiOutlineUser />
           </li>
         </ul>
