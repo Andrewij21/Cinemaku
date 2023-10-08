780375a00cac6b1ae09f33333d7f72b7edf1ecb1
Andrewij21 <andrewijaya988@gmail.com>
2023-10-08 18:48:24 +0700

    feat: create medialist component

---
+++ src/components/media/MediaList.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import MediaItems from "./MediaItems";
+
+const MediaList = ({ tranding }) => {
+  return (
+    <div className="">
+      <h2 className="text-2xl text-slate-200 mb-4">Tranding</h2>
+      <div className="text-white flex flex-wrap w-full max-w-full  ">
+        {tranding.map((movie) => {
+          return <MediaItems key={movie.id} movie={movie} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+MediaList.propTypes = {
+  tranding: PropTypes.array,
+};
+
+export default MediaList;
