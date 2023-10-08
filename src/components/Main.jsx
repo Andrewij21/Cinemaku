4872ef5e5df862e6d84588f6d5ef20cceed58fee
Andrewij21 <andrewijaya988@gmail.com>
2023-10-08 18:48:56 +0700

    refactor: use medialist and limit movie data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 // import avanger from "../../public/avanger.jpg";
-import { getMovieTrending } from "../api/movieApi";
+import { getTrending } from "../api/mediaApi";
+import MediaList from "./media/MediaList";
 
 const Main = () => {
   const [tranding, setTrending] = useState([]);
   useEffect(() => {
     let isMounted = false;
-    getMovieTrending()
+    getTrending()
       .then((res) => {
         isMounted = true;
         console.log(res);
-        const movies = res.data.results;
+        const movies = res.data.results.slice(0, 6);
         console.log({ movies });
         isMounted && setTrending(movies);
       })
@@ -23,33 +24,7 @@ const Main = () => {
   console.log({ tranding });
   return (
     <main className="">
-      <h2 className="text-2xl text-slate-200 mb-4">Tranding</h2>
-      <div className="text-white flex flex-wrap w-full max-w-full   ">
-        {tranding.map((movie) => {
-          return (
-            <div
-              className="items-start h-auto rounded-xl overflow-hidden w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2 flex flex-col"
-              key={movie.id}
-            >
-              <div className="h-80 mx-auto   ">
-                <img
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  // className=" bg-red-400 object-cover"
-                  className=" h-full w-full rounded-lg object-contain  object-center"
-                  alt={movie.title}
-                />
-              </div>
-              <div className="text-center py-4 mx-auto">
-                <h4 className="text-ellipsis overflow-hidden">{movie.title}</h4>
-                <span className="block mt-2">
-                  ({movie.release_date.substr(0, 4)})
-                </span>
-                <span className="block">{movie.vote_average}</span>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <MediaList tranding={tranding} />
     </main>
   );
 };
