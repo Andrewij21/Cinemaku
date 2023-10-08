3a9d18b5d115c96ea6baea7034f9225dc2bc8326
Andrewij21 <andrewijaya988@gmail.com>
2023-10-08 21:58:36 +0700

    refactor: responsive tite

--- src/components/media/MediaList.jsx
+++ src/components/media/MediaList.jsx
@@ -4,7 +4,9 @@ import MediaItems from "./MediaItems";
 const MediaList = ({ tranding, title }) => {
   return (
     <div className="mt-4">
-      <h2 className="text-2xl text-slate-200 mb-4 capitalize">{title}</h2>
+      <h2 className="text-2xl text-slate-200 mb-4 text-center md:text-left capitalize">
+        {title}
+      </h2>
       <div className="text-white flex flex-wrap w-full max-w-full  ">
         {tranding.map((movie) => {
           return <MediaItems key={movie.id} movie={movie} />;
