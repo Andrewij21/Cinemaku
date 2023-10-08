25cb20215a2662f12d3d54a82bc180a6cce8d305
Andrewij21 <andrewijaya988@gmail.com>
2023-10-08 21:53:57 +0700

    refactor: add margin top

--- src/components/media/MediaList.jsx
+++ src/components/media/MediaList.jsx
@@ -3,7 +3,7 @@ import MediaItems from "./MediaItems";
 
 const MediaList = ({ tranding, title }) => {
   return (
-    <div className="">
+    <div className="mt-4">
       <h2 className="text-2xl text-slate-200 mb-4 capitalize">{title}</h2>
       <div className="text-white flex flex-wrap w-full max-w-full  ">
         {tranding.map((movie) => {
