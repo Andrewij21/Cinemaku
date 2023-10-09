f73a980d215d576e570951c29e2db9b80f260566
Andrewij21 <andrewijaya988@gmail.com>
2023-10-09 15:33:43 +0700

    feat: add seemore path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./page/Home";
+import SeeMore from "./page/SeeMore";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="" element={<Home />} />
+        <Route path="/seemore" element={<SeeMore />} />
       </Route>
     </Routes>
   );
