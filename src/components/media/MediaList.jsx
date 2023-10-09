91d4d69065aa431d29f80ecd4cd379ae319c43d5
Andrewij21 <andrewijaya988@gmail.com>
2023-10-09 10:56:02 +0700

    feat: add see more text

--- src/components/media/MediaList.jsx
+++ src/components/media/MediaList.jsx
@@ -4,8 +4,11 @@ import MediaItems from "./MediaItems";
 const MediaList = ({ topic, title }) => {
   return (
     <div className="mt-4">
-      <h2 className="text-2xl text-slate-200 mb-4 text-center md:text-left capitalize">
+      <h2 className="text-2xl text-slate-200 mb-4 text-center md:text-left capitalize relative">
         {title}
+        <span className="text-sm absolute right-0 bottom-0 cursor-pointer hover:text-rose-600">
+          see more
+        </span>
       </h2>
       <div className="text-white flex flex-wrap w-full max-w-full  ">
         {topic.map((movie) => {
