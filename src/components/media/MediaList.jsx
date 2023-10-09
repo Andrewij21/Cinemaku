4fc3c45e67ba00f9c304f111a562adae29ec2f7d
Andrewij21 <andrewijaya988@gmail.com>
2023-10-09 15:33:14 +0700

    feat: link to seemore page

--- src/components/media/MediaList.jsx
+++ src/components/media/MediaList.jsx
@@ -1,13 +1,20 @@
 import PropTypes from "prop-types";
 import MediaItems from "./MediaItems";
+import { Link } from "react-router-dom";
 
 const MediaList = ({ topic, title }) => {
   return (
     <div className="mt-4">
       <h2 className="text-2xl text-slate-200 mb-4 text-center md:text-left capitalize relative">
         {title}
-        <span className="text-sm absolute right-0 bottom-0 cursor-pointer hover:text-rose-600">
-          see more
+        <span>
+          <Link
+            to={"/seemore"}
+            state={{ title }}
+            className="text-sm absolute right-0 bottom-0 cursor-pointer hover:text-rose-600"
+          >
+            see more
+          </Link>
         </span>
       </h2>
       <div className="text-white flex flex-wrap w-full max-w-full  ">
