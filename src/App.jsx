8cbb156d466449d72f28253d40055b477f459782
Andrewij21 <andrewijaya988@gmail.com>
2023-10-12 16:19:35 +0700

    feat: add detail page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./page/Home";
 import SeeMore from "./page/SeeMore";
+import Detail from "./page/Detail";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route path="" element={<Home />} />
         <Route path="/seemore" element={<SeeMore />} />
+        <Route path="/detail" element={<Detail />} />
       </Route>
     </Routes>
   );
---
+++ src/page/Detail.jsx
@@ -0,0 +1,33 @@
+const Detail = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h2>Raya and the last dragon</h2>
+        {/* list of category */}
+        <ul>
+          <li>comady</li>
+          <li>action</li>
+          <li>drama</li>
+        </ul>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+          iusto adipisci? Consectetur accusamus sed illum ea commodi optio
+          repudiandae ipsam.
+        </p>
+        <div>
+          <h4>cast</h4>
+          <ul>
+            <li>user1</li>
+            <li>user2</li>
+            <li>user3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
